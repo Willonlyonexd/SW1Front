0b855a3e54b6f3ab42eacb01e2db6658416a5b43
erickAricari <erickaricari@gmail.com>
2024-10-07 00:38:43 -0400

    Update Socket.IO URL to use backend on Render

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,6 +1,5 @@
 import { io } from 'socket.io-client';
 
 // Conectarse al servidor de Socket.IO (ajusta la URL según sea necesario)
-const socket = io('https://backendconbd.onrender.com');  // Asegúrate de que la URL corresponda al backend
-
+const socket = io('https://tu-backend-en-render.com');
 export default socket;
